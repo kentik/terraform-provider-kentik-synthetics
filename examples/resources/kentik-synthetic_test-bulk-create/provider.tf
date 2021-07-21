1c062af49763c49cf3bccd4f7a93ba9973d557c5
ziemowit141 <ziemowit141@gmail.com>
2021-07-21 12:44:22 +0200

    Changing version constraint to also match minor version

--- examples/resources/kentik-synthetic_test-bulk-create/provider.tf
+++ examples/resources/kentik-synthetic_test-bulk-create/provider.tf
@@ -1,7 +1,7 @@
 terraform {
   required_providers {
     kentik-synthetics = {
-      version = "~> 0.1.0"
+      version = ">= 0.1.0"
       source  = "kentik.com/automation/kentik-synthetics"
     }
   }
