abcdb8b8aaa2b9b6d5be27adbd69656147777375
ziemowit141 <ziemowit141@gmail.com>
2021-08-05 14:58:20 +0200

    More verbose variable names

--- synthetics/agents_distance_filter.go
+++ synthetics/agents_distance_filter.go
@@ -8,10 +8,10 @@ import (
 func filterAgentsByDistance(agents []synthetics.V202101beta1Agent,
 	lat float64, long float64, distConstr float64) []synthetics.V202101beta1Agent {
 	var filteredAgents []synthetics.V202101beta1Agent
-	p := geo.NewPoint(lat, long)
+	referencePoint := geo.NewPoint(lat, long)
 	for _, agent := range agents {
-		p2 := geo.NewPoint(*agent.Lat, *agent.Long)
-		dist := p.GreatCircleDistance(p2)
+		agentCoordinates := geo.NewPoint(*agent.Lat, *agent.Long)
+		dist := referencePoint.GreatCircleDistance(agentCoordinates)
 		if dist < distConstr {
 			filteredAgents = append(filteredAgents, agent)
 		}
