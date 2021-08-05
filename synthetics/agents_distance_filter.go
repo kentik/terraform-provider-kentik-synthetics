18571008520e1cb3f23fc573fd0cd15821728b49
ziemowit141 <ziemowit141@gmail.com>
2021-08-05 14:57:03 +0200

    Fixing linter errors

--- synthetics/agents_distance_filter.go
+++ synthetics/agents_distance_filter.go
@@ -1,11 +1,12 @@
 package synthetics
 
 import (
-	"github.com/kellydunn/golang-geo"
+	geo "github.com/kellydunn/golang-geo"
 	"github.com/kentik/community_sdk_golang/apiv6/kentikapi/synthetics"
 )
 
-func filterAgentsByDistance(agents []synthetics.V202101beta1Agent, lat float64, long float64, distConstr float64) []synthetics.V202101beta1Agent {
+func filterAgentsByDistance(agents []synthetics.V202101beta1Agent,
+	lat float64, long float64, distConstr float64) []synthetics.V202101beta1Agent {
 	var filteredAgents []synthetics.V202101beta1Agent
 	p := geo.NewPoint(lat, long)
 	for _, agent := range agents {
