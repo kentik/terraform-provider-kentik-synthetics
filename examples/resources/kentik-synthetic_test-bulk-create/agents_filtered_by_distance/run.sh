7cc6f5c2d3204659070fa950f9008a9b507fc86f
ziemowit141 <ziemowit141@gmail.com>
2021-08-09 11:26:51 +0200

    Typo in test server port

--- examples/resources/kentik-synthetic_test-bulk-create/agents_filtered_by_distance/run.sh
+++ examples/resources/kentik-synthetic_test-bulk-create/agents_filtered_by_distance/run.sh
@@ -7,7 +7,7 @@
 SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
 REPO_DIR=$(cd -- "$SCRIPT_DIR" && cd ../../../../ && pwd)
 
-TEST_API_SERVER_ENDPOINT=${TEST_API_SERVER_ENDPOINT:-"localhost:8080"}
+TEST_API_SERVER_ENDPOINT=${TEST_API_SERVER_ENDPOINT:-"localhost:9955"}
 
 source "$REPO_DIR/tools/utility_functions.sh" || exit 1
 
