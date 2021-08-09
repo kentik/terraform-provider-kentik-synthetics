356e9c4be3fa3ec2163caa2662b272f4b9d6b593
ziemowit141 <ziemowit141@gmail.com>
2021-08-09 11:19:02 +0200

    Usage example was moved to /examples/resources/kentic_synthetic_test-bulk-create

--- examples/data-sources/kentik-synthetics_agents/data-source.tf
+++ examples/data-sources/kentik-synthetics_agents/data-source.tf
@@ -1,8 +1,4 @@
-data "kentik-synthetics_agents" "agents" {
-  latitude  = 50.55
-  longitude = 5.5
-  distance  = 1000
-}
+data "kentik-synthetics_agents" "agents" {}
 
 output "agents_list" {
   value = data.kentik-synthetics_agents.agents
