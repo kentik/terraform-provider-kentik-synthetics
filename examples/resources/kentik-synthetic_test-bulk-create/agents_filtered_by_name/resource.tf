2ac103872991f3262959f288873909b4aabfe44e
ziemowit141 <ziemowit141@gmail.com>
2021-08-10 10:31:31 +0200

    Minimising resurce file in the examples

--- examples/resources/kentik-synthetic_test-bulk-create/agents_filtered_by_name/resource.tf
+++ examples/resources/kentik-synthetic_test-bulk-create/agents_filtered_by_name/resource.tf
@@ -10,7 +10,7 @@ locals {
 }
 
 resource "kentik-synthetics_test" "private-agents-test" {
-  name      = "private-agents-test"
+  name      = "name-filtered-agents-test"
   type      = "hostname"
   device_id = "75702"
   status    = "TEST_STATUS_ACTIVE"
@@ -19,33 +19,10 @@ resource "kentik-synthetics_test" "private-agents-test" {
       target = "www.example.com"
     }
     agent_ids = local.agents_ids
-    period = 61
-    count  = 2
-    expiry = 5001
-    limit  = 4
     tasks = tolist([
       "ping",
       "traceroute"
     ])
-    health_settings {
-      latency_critical      = 1
-      latency_warning       = 2
-      packet_loss_critical  = 3
-      packet_loss_warning   = 4
-      jitter_critical       = 5
-      jitter_warning        = 6
-      http_latency_critical = 7
-      http_latency_warning  = 8
-      http_valid_codes = tolist([
-        200,
-        201
-      ])
-      dns_valid_codes = tolist([
-        1,
-        2,
-        3
-      ])
-    }
     monitoring_settings {
       activation_grace_period = "2"
       activation_time_unit    = "m"
@@ -68,12 +45,6 @@ resource "kentik-synthetics_test" "private-agents-test" {
     port     = 443
     protocol = "icmp"
     family   = "IP_FAMILY_V6"
-    servers = tolist([
-      "server-one",
-      "server-two",
-    ])
-    use_local_ip = true
-    reciprocal   = false
     rollup_level = 1
   }
 }
