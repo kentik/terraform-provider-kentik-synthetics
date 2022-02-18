5f05d6dd2a6a0b8898a267ec335ed56454c6fa5e
Jan Dziurzynski <jddziurzynski@gmail.com>
2022-02-18 14:30:43 +0100

    deduplicate provider.tf files

---
+++ examples/common/provider.tf
@@ -0,0 +1,11 @@
+# Note: configuration for locally-built provider
+terraform {
+  required_providers {
+    kentik-synthetics = {
+      version = ">= 0.1.0"
+      source  = "kentik/automation/kentik-synthetics"
+    }
+  }
+}
+
+provider "kentik-synthetics" {}
